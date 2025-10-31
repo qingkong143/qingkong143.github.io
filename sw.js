e6586d7b53e272c5c1a6efe02b35111801511d71
qingkong143 <3186708635@qq.com>
2025-10-31 21:00:55 +0800

    Site updated: YYYY-10-2025年10月31日 21:00:55

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1761904130211";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1761915653278";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
