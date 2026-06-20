43e34fe57f2f10b5d844e1befe8791b5abe63f37
qingkong143 <3186708635@qq.com>
2026-06-20 16:32:48 +0800

    添加了sitemap选项

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import remarkGfm from 'remark-gfm';
@@ -5,7 +6,7 @@ import remarkLinkCard from './src/plugins/remark-link-card.mjs';
 
 export default defineConfig({
   site: 'https://blog.318670.xyz',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     remarkPlugins: [remarkGfm, remarkLinkCard],
     shikiConfig: {
